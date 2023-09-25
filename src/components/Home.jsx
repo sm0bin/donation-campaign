93d434d86339872ab5805dbe371d18d1ecae0f8f
GhostMobin <s1810677110@ru.ac.bd>
2023-09-25 22:42:22 +0600

    images add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,8 @@ import "../assets/Clothing.png";
 const Home = () => {
   return (
     <div className="mx-4 md:mx-8 lg:mx-36">
-      <div className="bg-[url('./assets/Clothing.png')] bg-no-repeat bg-cover bg-opacity-5">
+      <div className="">
+        {/* <div className="bg-[url('./assets/Clothing.png')] bg-no-repeat bg-cover bg-opacity-5"> */}
         <Header />
         <Banner />
       </div>
