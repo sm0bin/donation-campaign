1483fe5b073ea8a472b666173b5d861fba366064
GhostMobin <s1810677110@ru.ac.bd>
2023-09-26 21:17:25 +0600

    See all button functional

--- src/components/YourDonations.jsx
+++ src/components/YourDonations.jsx
@@ -6,6 +6,7 @@ import { getMyDonations } from "./utility/localStorage";
 const YourDonations = () => {
   const allDonations = useLoaderData();
   const [myDonations, setMyDonations] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
     const myStoredDonations = getMyDonations();
@@ -20,11 +21,20 @@ const YourDonations = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {myDonations.map((item) => (
+        {myDonations.slice(0, dataLength).map((item) => (
           <YourDonation key={item.id} item={item} />
         ))}
       </div>
-      <button className="px-6 py-4 bg-green-500 font-semibold text-white rounded-lg mx-auto block mt-10 hover:bg-green-600">
+
+      <button
+        className={`px-6 py-4 bg-green-500 font-semibold text-white rounded-lg mx-auto mt-10 hover:bg-green-600 
+        ${
+          myDonations.length <= 4 || dataLength === myDonations.length
+            ? "hidden"
+            : "block"
+        }`}
+        onClick={() => setDataLength(myDonations.length)}
+      >
         See All
       </button>
     </>
